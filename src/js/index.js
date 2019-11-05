ec173a81db2af6dd33938510bff063e614bf2eee
vert7 <evertponce@outlook.com>
2019-11-05 01:40:02 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,33 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let excuseDiv = document.querySelector("#excuse");
+  excuseDiv.innerHTML = generateExcuse();
 };
+let generateExcuse = function() {
+  let who = [
+    " a dog",
+    " the baby-sister",
+    " a pet Python",
+    " my roomates guests"
+  ];
+  let what = [
+    " ATE my homework",
+    " peed on my homework",
+    " ravaged my homework",
+    " owned my homework"
+  ];
+  let when = [
+    " yesterday",
+    " hours ago",
+    " right before I got here",
+    " last night"
+  ];
+
+  return (
+    who[random(0, 4)] + "" + what[random(0, 4)] + "" + when[random(0, 4)] + ""
+  );
+};
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
